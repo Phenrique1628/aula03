fa62a8ae89be1b65cce690e9902acf15baf98380
Phenrique1628 <henriquepedro02964@gmail.com>
2024-11-18 16:09:39 -0400

    Criação dos filtros

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,34 @@ export default function Home() {
         receberListaProdutos();
     }, []);
 
+    const ordenarDeAaZ = () =>{
+        const ListaAux = [...lista].sort((a,b) => a.title.localeCompare(b.title));
+        setLista(ListaAux);
+    }
+
+    const ordenarDeZaA = () =>{
+        const ListaAux = [...lista].sort((a,b) => b.title.localeCompare(a.title));
+        setLista(ListaAux);
+    }
+
+    const PrecoMaiorMenor = () =>{
+        const ListaAux = [...lista].sort((a,b) => b.price - a.price);
+        setLista(ListaAux);
+    }
+
+    const PrecoMenorMaior = () =>{
+        const ListaAux = [...lista].sort((a,b) => a.price - b.price);
+        setLista(ListaAux);
+    }
+
  
     return (
         <>
             <h1 className={styles.titulo}>Lista dos Produtos</h1>
+            <button className={styles.botao} onClick={() => ordenarDeAaZ()}>A-Z</button>
+            <button className={styles.botao} onClick={() => ordenarDeZaA()}>Z-A</button>
+            <button className={styles.botao} onClick={() => PrecoMaiorMenor()}>Maior - Menor</button>
+            <button className={styles.botao} onClick={() => PrecoMenorMaior()}>Menor - Maior</button>
             <ListaProdutos lista={lista} />
         </>
     );
